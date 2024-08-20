3f5565305120d2a49bf2bce335832361df17d45b
Makadur4 <rm90349@estudante.fieb.edu.br>
2024-08-20 16:40:40 -0300

    final

---
+++ src/shared/Pizzas.jsx
@@ -0,0 +1,75 @@
+import bacon from "../assets/images/bacon.png" 
+import calabresa from "../assets/images/calabresa.png" 
+import frango from "../assets/images/frango.png" 
+import havaiana from "../assets/images/havaiana.png" 
+import margherita from "../assets/images/margherita.png" 
+import mexicana from "../assets/images/mexicana.png" 
+import pepperoni from "../assets/images/pepperoni.png" 
+import portuguesa from "../assets/images/portuguesa.png" 
+import quatroqueijos from "../assets/images/quatroqueijos.png" 
+import vegetariana from "../assets/images/vegetariana.png" 
+
+const Pizzas = [ 
+    { 
+    "id": 1, 
+    "nome": "Margherita", 
+    "descricao": "Molho de tomate, mussarela e manjericão fresco.", 
+    "imagem": margherita
+}, 
+{ 
+"id": 2, 
+"nome": "Pepperoni", 
+"descricao": "Molho de tomate, mussarela e pepperoni.", 
+"imagem":  pepperoni
+}, 
+{ 
+"id": 3, 
+"nome": "Quatro Queijos", 
+"descricao": "Molho de tomate, mussarela, gorgonzola, parmesão e provolone.", 
+"imagem": quatroqueijos
+}, 
+{ 
+"id": 4, 
+"nome": "Frango com Catupiry", 
+"descricao": "Molho de tomate, mussarela, frango desfiado e catupiry.", 
+"imagem": frango
+}, 
+{ 
+"id": 5, 
+"nome": "Portuguesa", 
+"descricao": "Molho de tomate, mussarela, presunto, ovo, cebola, azeitona e orégano.", 
+"imagem": portuguesa
+}, 
+{ 
+"id": 6, 
+"nome": "Calabresa", 
+"descricao": "Molho de tomate, mussarela, calabresa e cebola.", 
+"imagem": calabresa 
+}, 
+{ 
+"id": 7, 
+"nome": "Vegetariana", 
+"descricao": "Molho de tomate, mussarela, pimentão, cebola, azeitona, champignon e tomate.", 
+"imagem": vegetariana
+}, 
+{ 
+"id": 8, 
+"nome": "Havaiana", 
+"descricao": "Molho de tomate, mussarela, presunto e abacaxi.", 
+"imagem":havaiana
+}, 
+{ 
+"id": 9, 
+"nome": "Bacon", 
+"descricao": "Molho de tomate, mussarela, bacon e cebola.", 
+"imagem":bacon
+}, 
+{ 
+"id": 10, 
+"nome": "Mexicana", 
+"descricao": "Molho de tomate, mussarela, carne moída, pimenta jalapeño, cebola e coentro.", 
+"imagem": mexicana
+} 
+] 
+ 
+export default Pizzas;
